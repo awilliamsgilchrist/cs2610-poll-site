ddc3c521e127e308c63773f81c23ed9ba56bc391
Andrew Gilchrist <andrew.williams.gilchrist@aggiemail.usu.edu>
2017-04-15 20:32:27 +0000

    Achieved functionality consistent with step 4 of the tutorial

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 from .models import Question, Choice
 
 # Create your views here.
-def index(request):
-    latestQuestionList = Question.objects.order_by('-datePublished')[:5]
-    context = {
-        'latestQuestionList' : latestQuestionList,
-    }
-    return render(request, 'polls/index.html', context)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/details.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latestQuestionList'
     
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
     
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
